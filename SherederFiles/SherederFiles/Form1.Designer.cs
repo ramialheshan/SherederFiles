d35b493a830e01c3a6fc98aa31f943adda6084ce
Rami Alheshan <47783384+ramialheshan@users.noreply.github.com>
2022-02-26 00:14:40 +0200

    Add files via upload
    Load all files

---
+++ SherederFiles/SherederFiles/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace SherederFiles
+{
+    partial class FormShreder
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormShreder));
+            this.StartRecycle = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.PathFolder = new System.Windows.Forms.TextBox();
+            this.OpenFolder = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.StateLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.ProcessShrederFiles = new System.Windows.Forms.ToolStripProgressBar();
+            this.CountLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.CountOfFile = new System.Windows.Forms.ToolStripStatusLabel();
+            this.CountClearedFilesLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.CountClearedFiles = new System.Windows.Forms.ToolStripStatusLabel();
+            this.StateRemove = new System.Windows.Forms.CheckBox();
+            this.NideTurnOffChecked = new System.Windows.Forms.CheckBox();
+            this.panel1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // StartRecycle
+            // 
+            this.StartRecycle.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.StartRecycle.Location = new System.Drawing.Point(451, 18);
+            this.StartRecycle.Name = "StartRecycle";
+            this.StartRecycle.Size = new System.Drawing.Size(122, 34);
+            this.StartRecycle.TabIndex = 0;
+            this.StartRecycle.Text = "Старт очистки";
+            this.StartRecycle.UseVisualStyleBackColor = true;
+            this.StartRecycle.Click += new System.EventHandler(this.StartRecycle_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Каталог очистки:";
+            // 
+            // PathFolder
+            // 
+            this.PathFolder.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PathFolder.Location = new System.Drawing.Point(0, 0);
+            this.PathFolder.Margin = new System.Windows.Forms.Padding(0);
+            this.PathFolder.Name = "PathFolder";
+            this.PathFolder.Size = new System.Drawing.Size(393, 20);
+            this.PathFolder.TabIndex = 2;
+            // 
+            // OpenFolder
+            // 
+            this.OpenFolder.Dock = System.Windows.Forms.DockStyle.Right;
+            this.OpenFolder.Location = new System.Drawing.Point(393, 0);
+            this.OpenFolder.Margin = new System.Windows.Forms.Padding(0);
+            this.OpenFolder.Name = "OpenFolder";
+            this.OpenFolder.Size = new System.Drawing.Size(25, 20);
+            this.OpenFolder.TabIndex = 0;
+            this.OpenFolder.Text = "...";
+            this.OpenFolder.UseVisualStyleBackColor = true;
+            this.OpenFolder.Click += new System.EventHandler(this.OpenFolder_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.PathFolder);
+            this.panel1.Controls.Add(this.OpenFolder);
+            this.panel1.Location = new System.Drawing.Point(15, 26);
+            this.panel1.Margin = new System.Windows.Forms.Padding(0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(418, 20);
+            this.panel1.TabIndex = 3;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.StateLabel,
+            this.ProcessShrederFiles,
+            this.CountLabel,
+            this.CountOfFile,
+            this.CountClearedFilesLabel,
+            this.CountClearedFiles});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 73);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(585, 23);
+            this.statusStrip1.TabIndex = 4;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // StateLabel
+            // 
+            this.StateLabel.Name = "StateLabel";
+            this.StateLabel.Size = new System.Drawing.Size(109, 18);
+            this.StateLabel.Text = "Состояние очистки:";
+            // 
+            // ProcessShrederFiles
+            // 
+            this.ProcessShrederFiles.Margin = new System.Windows.Forms.Padding(1, 5, 1, 3);
+            this.ProcessShrederFiles.Name = "ProcessShrederFiles";
+            this.ProcessShrederFiles.Size = new System.Drawing.Size(100, 15);
+            this.ProcessShrederFiles.Step = 1;
+            // 
+            // CountLabel
+            // 
+            this.CountLabel.Margin = new System.Windows.Forms.Padding(25, 3, 0, 2);
+            this.CountLabel.Name = "CountLabel";
+            this.CountLabel.Size = new System.Drawing.Size(112, 18);
+            this.CountLabel.Text = "Количество файлов:";
+            // 
+            // CountOfFile
+            // 
+            this.CountOfFile.Name = "CountOfFile";
+            this.CountOfFile.Size = new System.Drawing.Size(0, 18);
+            // 
+            // CountClearedFilesLabel
+            // 
+            this.CountClearedFilesLabel.Margin = new System.Windows.Forms.Padding(50, 3, 0, 2);
+            this.CountClearedFilesLabel.Name = "CountClearedFilesLabel";
+            this.CountClearedFilesLabel.Size = new System.Drawing.Size(58, 18);
+            this.CountClearedFilesLabel.Text = "Очищено:";
+            // 
+            // CountClearedFiles
+            // 
+            this.CountClearedFiles.Name = "CountClearedFiles";
+            this.CountClearedFiles.Size = new System.Drawing.Size(0, 18);
+            // 
+            // StateRemove
+            // 
+            this.StateRemove.AutoSize = true;
+            this.StateRemove.Location = new System.Drawing.Point(15, 49);
+            this.StateRemove.Name = "StateRemove";
+            this.StateRemove.Size = new System.Drawing.Size(182, 17);
+            this.StateRemove.TabIndex = 5;
+            this.StateRemove.Text = "Удалять файлы после очистки";
+            this.StateRemove.UseVisualStyleBackColor = true;
+            // 
+            // NideTurnOffChecked
+            // 
+            this.NideTurnOffChecked.AutoSize = true;
+            this.NideTurnOffChecked.Location = new System.Drawing.Point(203, 49);
+            this.NideTurnOffChecked.Name = "NideTurnOffChecked";
+            this.NideTurnOffChecked.Size = new System.Drawing.Size(143, 17);
+            this.NideTurnOffChecked.TabIndex = 5;
+            this.NideTurnOffChecked.Text = "Выключить компьютер";
+            this.NideTurnOffChecked.UseVisualStyleBackColor = true;
+            // 
+            // FormShreder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(585, 96);
+            this.Controls.Add(this.NideTurnOffChecked);
+            this.Controls.Add(this.StateRemove);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.StartRecycle);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormShreder";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "_";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button StartRecycle;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox PathFolder;
+        private System.Windows.Forms.Button OpenFolder;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel StateLabel;
+        private System.Windows.Forms.ToolStripProgressBar ProcessShrederFiles;
+        private System.Windows.Forms.ToolStripStatusLabel CountLabel;
+        private System.Windows.Forms.ToolStripStatusLabel CountOfFile;
+        private System.Windows.Forms.ToolStripStatusLabel CountClearedFilesLabel;
+        private System.Windows.Forms.ToolStripStatusLabel CountClearedFiles;
+        private System.Windows.Forms.CheckBox StateRemove;
+        private System.Windows.Forms.CheckBox NideTurnOffChecked;
+    }
+}
+
---
+++ SherederFiles/SherederFiles/Form1.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+using System.Management;
+using System.Diagnostics;
+
+
+  
+
+namespace SherederFiles
+{
+    public partial class FormShreder : Form
+    {
+        //Блок инициализации 
+
+        #region Описание переменных формы 
+
+        BackgroundWorker BackgroundWorker1;
+        bool freeBackgroundWorker1 = true;
+        BackgroundWorker BackgroundWorker2;
+        bool freeBackgroundWorker2 = true;
+        BackgroundWorker BackgroundWorker3;
+        bool freeBackgroundWorker3 = true;
+        BackgroundWorker BackgroundWorkerMain;
+        bool Working;
+        string[] filePaths;
+        int countDeletedFiles;
+
+        DateTime StartClear;
+
+
+        #endregion
+
+        #region Блок инициализации 
+        
+        public FormShreder()
+        {
+            InitializeComponent();
+
+            BackgroundWorker1 = new BackgroundWorker();
+            BackgroundWorker1.DoWork += new DoWorkEventHandler(BackgroundWorker_DoWork);
+            BackgroundWorker1.RunWorkerCompleted+=new RunWorkerCompletedEventHandler(BackgroundWorker1_RunWorkerCompleted);
+            BackgroundWorker2 = new BackgroundWorker();
+            BackgroundWorker2.DoWork += new DoWorkEventHandler(BackgroundWorker_DoWork);
+            BackgroundWorker2.RunWorkerCompleted+=new RunWorkerCompletedEventHandler(BackgroundWorker2_RunWorkerCompleted);
+            BackgroundWorker3 = new BackgroundWorker();
+            BackgroundWorker3.DoWork += new DoWorkEventHandler(BackgroundWorker_DoWork);
+            BackgroundWorker3.RunWorkerCompleted += new RunWorkerCompletedEventHandler(BackgroundWorker3_RunWorkerCompleted);
+            BackgroundWorkerMain = new BackgroundWorker();
+            BackgroundWorkerMain.DoWork += new DoWorkEventHandler(BackgroundWorkerMain_DoWork);
+        }
+
+        #endregion
+
+        //------------------------------------------------------------------------------------------------------------
+
+        //Методы формы 
+
+        #region Метод: Выключить компьютер 
+        
+        void Shutdown()
+        {
+            Process.Start("shutdown.exe", "-s -t 0");
+        }
+
+        void ShutdownVer2()
+        {
+            ManagementBaseObject mboShutdown = null;
+            ManagementClass mcWin32 = new ManagementClass("Win32_OperatingSystem");
+            mcWin32.Get();
+
+            // You can't shutdown without security privileges
+            mcWin32.Scope.Options.EnablePrivileges = true;
+            ManagementBaseObject mboShutdownParams =
+                     mcWin32.GetMethodParameters("Win32Shutdown");
+
+            // Flag 1 means we want to shut down the system. Use "2" to reboot.
+            mboShutdownParams["Flags"] = "1";
+            mboShutdownParams["Reserved"] = "0";
+            foreach (ManagementObject manObj in mcWin32.GetInstances())
+            {
+                mboShutdown = manObj.InvokeMethod("Win32Shutdown", mboShutdownParams, null);
+            }
+        }
+
+        #endregion
+
+        //------------------------------------------------------------------------------------------------------------
+
+        //События 
+
+        #region Событие: Запуск основного потока 
+        
+        void BackgroundWorkerMain_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while (Working)
+            {
+                Application.DoEvents();
+                if (countDeletedFiles == filePaths.Length){ break; }
+                if (freeBackgroundWorker1 && !BackgroundWorker1.IsBusy)
+                {
+                    try
+                    {
+                        BackgroundWorker1.RunWorkerAsync(filePaths[countDeletedFiles]);
+                        countDeletedFiles += 1;
+                    }
+                    catch (Exception) { }
+                }
+                if (freeBackgroundWorker2 && !BackgroundWorker2.IsBusy)
+                {
+                    try
+                    {
+                        BackgroundWorker2.RunWorkerAsync(filePaths[countDeletedFiles]);
+                        countDeletedFiles += 1;
+                    }
+                    catch (Exception) { }
+                }
+                if (freeBackgroundWorker3 && !BackgroundWorker3.IsBusy)
+                {
+                    try
+                    {
+                        BackgroundWorker3.RunWorkerAsync(filePaths[countDeletedFiles]);
+                        countDeletedFiles += 1;
+                    }
+                    catch (Exception) { }
+                }
+                Thread.Sleep(500);
+            }
+
+            #region Запись в LOG файл результатов очистки 
+
+            System.IO.FileStream fileLog = new FileStream(Application.StartupPath + "\\" + "LogFile_DateTime_" + DateTime.Now.ToString(), FileMode.CreateNew, FileAccess.ReadWrite, FileShare.Read);
+            
+            StreamWriter m_streamWriter = new StreamWriter(fileLog);
+            m_streamWriter.BaseStream.Seek(0, SeekOrigin.End);
+            m_streamWriter.WriteLine("Количество файлов для очистки: " + filePaths.Length.ToString());
+            m_streamWriter.WriteLine("Количество очищенных файлов: " + countDeletedFiles.ToString());
+            m_streamWriter.WriteLine("Время запуска очистки: " + StartClear.ToString());
+            m_streamWriter.WriteLine("Время остановки очистки: " + DateTime.Now.ToString());
+
+            #endregion
+
+            #region Проверка на выключение компа 
+
+            switch (NideTurnOffChecked.Checked)
+            {
+                case true:
+                    m_streamWriter.WriteLine("Время выключения ПК: " + DateTime.Now.ToString());
+                    m_streamWriter.Flush();
+                    fileLog.Flush();
+                    fileLog.Close();
+                    Shutdown(); break;
+                case false:
+                    m_streamWriter.Flush();
+                    fileLog.Flush();
+                    fileLog.Close();
+                    break;
+            }
+
+            #endregion
+
+            this.Invoke(new EventHandler(delegate { StartRecycle.Text = "Старт очистки"; }));
+            this.Invoke(new EventHandler(delegate { ProcessShrederFiles.Value = 0; }));
+            this.Invoke(new EventHandler(delegate { CountOfFile.Text = string.Empty; }));
+            this.Invoke(new EventHandler(delegate { CountClearedFiles.Text = string.Empty; })); 
+            this.Invoke(new EventHandler(delegate { PathFolder.Text = string.Empty; }));
+            this.Invoke(new EventHandler(delegate { StateRemove.Enabled = true; }));
+
+            MessageBox.Show("Очистка файлов завершена", "Информация", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        #endregion
+        
+        #region Событие: Запуск вспомогательной очистки 
+
+        void BackgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            FileClear((string)e.Argument);
+        }
+
+        #endregion
+
+        #region Событие: Закончилась обработка файла в потоке 
+
+        void BackgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            freeBackgroundWorker1 = true;
+        }
+        void BackgroundWorker2_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            freeBackgroundWorker2 = true;
+        }
+        void BackgroundWorker3_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            freeBackgroundWorker3 = true;
+        }
+
+        #endregion
+      
+        #region Событие: Начало очистки 
+
+        private void StartRecycle_Click(object sender, EventArgs e)
+        {
+            if (Directory.Exists(PathFolder.Text.Trim()))
+            {
+                filePaths = Directory.GetFiles(PathFolder.Text.Trim(), "*.*", SearchOption.AllDirectories);
+                CountOfFile.Text = filePaths.Length.ToString();
+                ProcessShrederFiles.Maximum = filePaths.Length;
+                countDeletedFiles = 0;
+
+                switch (Working)
+                {
+                    case true: 
+                        Working = false;
+                        StartRecycle.Text = "Старт очистки";
+                        StateRemove.Enabled = true;
+                        NideTurnOffChecked.Enabled = true;
+                        break;
+                               
+                    case false:
+                        StartClear = DateTime.Now;
+                        Working = true;
+                        StartRecycle.Text = "Остановить";
+                        StateRemove.Enabled = false;
+                        NideTurnOffChecked.Enabled = false;
+                        BackgroundWorkerMain.RunWorkerAsync();
+                        break;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Заданый каталог не существует", "Внимание!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation); 
+            }
+        }
+
+        #endregion
+
+        #region Событие: Выбор каталога 
+        
+        private void OpenFolder_Click(object sender, EventArgs e)
+        {
+            switch (folderBrowserDialog.ShowDialog())
+            {
+                case DialogResult.OK: PathFolder.Text = folderBrowserDialog.SelectedPath;
+                    break;
+            } 
+        }
+
+        #endregion
+
+        //------------------------------------------------------------------------------------------------------------
+
+
+        //Методы 
+
+        #region Метод: Очистка файла в потоке 
+
+        protected void FileClear(string PathFileForDelete)
+        {
+            FileStream streamCurrent = null;
+            try
+            {
+                streamCurrent = new FileStream(PathFileForDelete, FileMode.Open, FileAccess.ReadWrite, FileShare.None);
+                streamCurrent.Position = 0;
+                for (int q = 0; q < streamCurrent.Length; q++)
+                {
+                    streamCurrent.Position = q;
+                    streamCurrent.WriteByte(Convert.ToByte(0));
+                }
+
+                Thread.Sleep(100);
+                streamCurrent.Close();
+
+                if (StateRemove.Checked)
+                {
+                    try
+                    {
+                        System.IO.File.Delete(PathFileForDelete);
+                    }
+                    catch (Exception) { }
+                }
+
+                this.Invoke(new EventHandler(delegate { ProcessShrederFiles.Value += 1; }));
+                CountClearedFiles.Text = countDeletedFiles.ToString();
+            }
+            catch (Exception) { }
+        }
+
+        #endregion
+
+        //------------------------------------------------------------------------------------------------------------
+    }
+}
